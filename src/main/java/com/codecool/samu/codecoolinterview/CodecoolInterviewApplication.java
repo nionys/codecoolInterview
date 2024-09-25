99a1fb0b1656b92895ba956d36e5b6480742dc20
Samu <108835512+nionys@users.noreply.github.com>
2024-09-25 15:30:29 +0200

    initialize spring project

---
+++ src/main/java/com/codecool/samu/codecoolinterview/CodecoolInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.samu.codecoolinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecoolInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecoolInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/samu/codecoolinterview/CodecoolInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.samu.codecoolinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecoolInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
