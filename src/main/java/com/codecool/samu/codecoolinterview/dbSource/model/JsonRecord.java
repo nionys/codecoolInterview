4c84949863965e454130d336041c1af48c12caec
Samu <108835512+nionys@users.noreply.github.com>
2024-09-27 16:49:40 +0200

    add timestamp to jsonRecord

--- src/main/java/com/codecool/samu/codecoolinterview/dbSource/model/JsonRecord.java
+++ src/main/java/com/codecool/samu/codecoolinterview/dbSource/model/JsonRecord.java
@@ -1,8 +1,8 @@
 package com.codecool.samu.codecoolinterview.dbSource.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.CurrentTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -12,15 +12,13 @@ public class JsonRecord{
     @Id
     @GeneratedValue
     private long id;
+    @Basic(optional = false)
+    @Column(insertable = false, updatable = false)
+    @CurrentTimestamp
     private LocalDateTime time;
     private String json;
 
     public JsonRecord() {}
-    public JsonRecord(long id, LocalDateTime time, String json) {
-        this.id = id;
-        this.time = time;
-        this.json = json;
-    }
 
     public long getId() {
         return id;
