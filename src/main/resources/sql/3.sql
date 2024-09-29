7c45d08ee5e794a20c750b9378e5c18b608dadac
Samu <108835512+nionys@users.noreply.github.com>
2024-09-29 21:45:19 +0200

    add SQL solution for ex.3 and ex.4

---
+++ src/main/resources/sql/3.sql
@@ -0,0 +1,19 @@
+WITH helper AS (SELECT exam.module, MAX(exam.date) as lastdate
+                FROM held_exam
+                         LEFT JOIN exam on held_exam.exam_id = exam.id
+                         LEFT JOIN public.student student on exam.student_id = student.id
+                         LEFT JOIN public.person person on student.person_id = person.id
+                WHERE person.email = 'foo@bar.com'
+                GROUP BY exam.module)
+SELECT result.dimension, AVG(result.percentage), count(*)
+FROM result
+    LEFT JOIN held_exam on result.heldexam_id = held_exam.id
+    LEFT JOIN exam on held_exam.exam_id = exam.id
+    RIGHT JOIN helper on exam.module = helper.module
+WHERE exam.date = helper.lastdate
+GROUP BY result.dimension
+
+;
+
+
+
---
+++ src/main/resources/sql/4.sql
@@ -0,0 +1,15 @@
+SELECT email, examnumber, SUM(successful::int)::decimal/COUNT(*) as passrate
+FROM (
+    SELECT
+        person.email,
+        RANK () OVER (
+            PARTITION BY exam.module
+            ORDER BY exam.date
+            ) examnumber,
+        held_exam.successful
+    FROM held_exam
+        LEFT JOIN exam on held_exam.exam_id = exam.id
+        LEFT JOIN mentor on exam.mentor_id = mentor.id
+        LEFT JOIN person on mentor.person_id = person.id) as indexedexams
+GROUP BY email, examnumber;
+
